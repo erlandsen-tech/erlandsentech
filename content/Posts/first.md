bb5754db15dd412106daf14ac8faaa79d9f6e1a4
varleg <johninge@varleg.no>
2022-01-17 07:46:19 +0100

    init

---
+++ content/Posts/first.md
@@ -0,0 +1,10 @@
+title: Welcome
+date:  Sun Jan 16 18:50:25 CET 2022
+
+----
+
+## Humble begginings
+
+I am currently testing a new template engine. I am doing this because I will soon
+start my new master study on industrial economics and leadership. Therefore
+I need a new place to render my thoughts. This will be that space.
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = "John Inge Muldal Erlandsen"
+SITENAME = "Erlandsen.tech"
+SITETITLE = "Erlandsen.tech"
+SITESUBTITLE = "John's science and tech blog"
+SITEDESCRIPTION = "Writeups on math and programming."
+# SITELOGO = ''
+# FAVICON = '/images/favicon.ico'
+BROWSER_COLOR = "#333333"
+PYGMENTS_STYLE = "monokai"
+
+PATH = 'content'
+THEME = 'Flex'
+
+TIMEZONE = 'Europe/Oslo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'))
+
+# Social widget
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/john-inge-muldal-erlandsen-b11090175/'),
+          ('github', 'https://github.com/varleg'))
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
